7555e80ba882482c2896cc2d51d94c013b8a2814
Alex Bolodurin <shur026@yandex.ru>
2022-11-23 15:43:30 +0300

    Params added

--- src/main/java/ru/bolodurin/springmvc/controllers/FirstController.java
+++ src/main/java/ru/bolodurin/springmvc/controllers/FirstController.java
@@ -2,16 +2,20 @@ package ru.bolodurin.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class FirstController {
     @GetMapping("/")
-    public String helloPage(){
+    public String helloPage(@RequestParam (value = "name", required = false) String name,
+                            @RequestParam (value = "surname", required = false) String surname) {
+        System.out.printf("Fuck you, %s %s.%n", name, surname);
         return "first/hello";
     }
 
     @GetMapping("/bye")
-    public String byePage(){
+    public String byePage() {
         return "first/bye";
     }
 }
