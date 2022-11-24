c345593512de522fe604461e6022cc96bd498753
Alex Bolodurin <shur026@yandex.ru>
2022-11-24 16:33:40 +0300

    CRUD realization

---
+++ src/main/java/ru/bolodurin/springmvc/dao/PersonDAO.java
@@ -0,0 +1,34 @@
+package ru.bolodurin.springmvc.dao;
+
+import org.springframework.stereotype.Component;
+import ru.bolodurin.springmvc.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private final List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Andrew"));
+        people.add(new Person(++PEOPLE_COUNT, "Jane"));
+        people.add(new Person(++PEOPLE_COUNT, "Court"));
+        people.add(new Person(++PEOPLE_COUNT, "John"));
+        people.add(new Person(++PEOPLE_COUNT, "Anthony"));
+        people.add(new Person(++PEOPLE_COUNT, "Carl"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(new Person(0, "Person not found"));
+    }
+}
---
+++ src/main/java/ru/bolodurin/springmvc/models/Person.java
@@ -0,0 +1,21 @@
+package ru.bolodurin.springmvc.models;
+
+public class Person {
+    private final int id;
+    private final String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+}
